7a8fa7a701e6f574ec3ffd4d6b21f0434a61a8f6
Rayean Patric <75995758+rayeanpatric@users.noreply.github.com>
2023-10-02 21:49:13 +0530

    Editing

--- roadmap/src/App.js
+++ roadmap/src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
-import Logo from './Logo.js';
-=======
 import Logo from "./Logo";
->>>>>>> 80b521b54b8030e680e079c9793072256cb00120
 import './App.css'; // Create a CSS file for styling
 
 function App() {
